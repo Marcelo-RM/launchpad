caedfd8cce4bccfcb6d2534f8ba9a1233880f00e
Marcelo Rodrigues Montalvão <marcelordrgs98@gmail.com>
2019-09-12 18:04:23 -0300

    first commit

---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"master/Basic/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
---
+++ webapp/test/unit/controller/App.controller.js
@@ -0,0 +1,16 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"master/Basic/controller/App.controller"
+], function (Controller) {
+	"use strict";
+
+	QUnit.module("App Controller");
+
+	QUnit.test("I should test the App controller", function (assert) {
+		var oAppController = new Controller();
+		oAppController.onInit();
+		assert.ok(oAppController);
+	});
+
+});
