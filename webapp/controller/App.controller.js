11cddba50dd0d57b5db8a16aa60fcf34b182e08e
Marcelo Rodrigues Montalvão <marcelordrgs98@gmail.com>
2019-09-12 19:54:38 -0300

    adicionado iframe fixo

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,7 +5,19 @@ sap.ui.define([
 
 	return BaseController.extend("master.launchpad.controller.App", {
 		onInit: function () {
-
+			
+			var html = new sap.ui.core.HTML({
+				preferDOM: true,
+				content: "<iframe src='https://marcelo-rm.github.io/manageProjects/webapp' " + 
+							 " width='100%' height='99.3%' frameborder='0' name='iframe' " + 
+							 " scrolling='no' allowtransparency='true'></iframe>"
+			});
+			//this.getView().byId("page").addContent(html);
+			var x = new sap.m.Page({
+				content: html
+			});
+			this.getView().byId("pageContainer").addPage(x);
+			
 		}
 	});
 });
